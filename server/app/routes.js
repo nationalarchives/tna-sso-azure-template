bf620d0b4b5e81b30fdfb6f6c2b578475cd30782
Punal Chotrani <punalsc@gmail.com>
2019-02-19 18:24:51 +0000

    Added vue framework and express

---
+++ server/app/routes.js
@@ -0,0 +1,13 @@
+// require express
+const express = require("express");
+
+// create our router object
+const router = express.Router();
+
+// export our router
+module.exports = router;
+
+// route for our homepage
+router.get("/", function(req, res) {
+  res.render("pages/home");
+});
---
+++ server/server.js
@@ -0,0 +1,33 @@
+////////////// Express //////////////
+
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const bodyParser = require("body-parser");
+
+const cors = require("cors");
+const morgan = require("morgan");
+
+const app = express();
+const port = 5000;
+
+// Set the templating engine
+app.set("view engine", "ejs");
+app.use(expressLayouts);
+
+// Use body parser
+app.use(bodyParser.json());
+app.use(morgan("combine"));
+app.use(cors());
+
+// Route our app
+const router = require("./app/routes");
+app.use("/", router);
+
+// Set static files ( css & images etc )
+app.use(express.static(__dirname + "/public"));
+
+// Start the server
+app.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log("HTML only server is running");
+});
