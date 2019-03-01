b0fbb277b3be9e4be22a72e11af4037c48157f77
Punal Chotrani <punalsc@gmail.com>
2019-03-01 12:10:49 +0000

    Configuration

--- app.js
+++ app.js
@@ -3,43 +3,48 @@ const fs = require("fs");
 const path = require("path");
 const templatePath = "./assets/templates";
 const copydir = require("copy-dir");
-const outputCssFolder = "output/css";
-const outputImgFolder = "output/img";
+const outputDir = "output";
+const outputCssFolder = `${outputDir}/css`;
+const outputImgFolder = `${outputDir}/img`;
 
-fs.readdir(templatePath, function(err, files) {
+fs.readdir(templatePath, (err, files) => {
   if (err) {
     throw err;
   }
 
-  files.forEach(function(fileName) {
+  files.forEach(fileName => {
     let file = path.join(__dirname, templatePath, fileName),
       stats = fs.statSync(file);
 
     if (stats.isFile() && fileName !== ".DS_Store") {
-      fs.readFile(file, "UTF-8", function(err, contents) {
+      fs.readFile(file, "UTF-8", (err, contents) => {
         if (err) {
           throw err;
         }
 
-        const compiledFunction = pug.compileFile(file);
+        fs.rmdir(outputDir, () => {
+          fs.mkdir(outputDir, () => {
+            fs.mkdir(outputCssFolder, { recursive: true }, err => {
+              if (err) throw err;
+            });
+            fs.mkdir(outputImgFolder, { recursive: true }, err => {
+              if (err) throw err;
+            });
 
-        fs.mkdir(outputCssFolder, { recursive: true }, err => {
-          if (err) throw err;
+            const compiledFunction = pug.compileFile(file);
+
+            copydir.sync("assets/img", outputImgFolder);
+
+            fs.writeFile(
+              `${outputDir}/${fileName}.html`.replace(".pug", ""),
+              compiledFunction(),
+              err => {
+                if (err) throw err;
+                console.log("The file has been saved!");
+              }
+            );
+          });
         });
-        fs.mkdir(outputImgFolder, { recursive: true }, err => {
-          if (err) throw err;
-        });
-
-        copydir.sync("assets/img", outputImgFolder);
-
-        fs.writeFile(
-          `output/${fileName}.html`.replace(".pug", ""),
-          compiledFunction(),
-          err => {
-            if (err) throw err;
-            console.log("The file has been saved!");
-          }
-        );
       });
     }
   });
